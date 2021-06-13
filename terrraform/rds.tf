431ee714e8545077f2ea2f2fb973399fa7546364
Tuatini <email@tuatini.me>
2021-06-13 16:24:04 +0200

    Add RDS

--- terrraform/rds.tf
+++ terrraform/rds.tf
@@ -1,35 +1,45 @@
-# resource "random_password" "db_password" {
-#   length = 16
-#   special = false
-# }
+resource "random_password" "db_password" {
+  length = 16
+  special = false
+}
 
-# resource "aws_db_subnet_group" "default" {
-#   name = "kubeflow-${var.vpc_name}-db"
+resource "aws_security_group" "pg_security_group" {
+  name = "argoflow_db_security_group"
+  vpc_id = module.vpc.vpc_id
+}
 
-#   subnet_ids = var.private_subnets_ids
+resource "aws_security_group_rule" "allow_postgres_inbound" {
+  type = "ingress"
+  from_port = 5432
+  to_port = 5432
+  protocol = "tcp"
+  security_group_id = aws_security_group.pg_security_group.id
+  # Only allow from eks cluster
+  source_security_group_id = module.eks.cluster_primary_security_group_id
+}
 
-#   tags = {
-#     Name = "Kubeflow DB subnet group"
-#   }
-# }
+resource "aws_db_subnet_group" "default" {
+  name = "argoflow-${module.vpc.name}-db"
+  subnet_ids = module.vpc.private_subnets
 
-# resource "aws_db_instance" "kubeflow-db-instance-1" {
-#   identifier = "kubeflow-${var.vpc_name}-db"
-#   allocated_storage = 20
-#   storage_type = "gp2"
-#   engine = "postgres"
-#   engine_version = "12.5"
-#   instance_class = var.db_instance_type
-#   name = "kubeflow_db"
-#   username = "postgres"
-#   password = random_password.db_password.result
+  tags = {
+    Name = "Argoflow DB subnet group"
+  }
+}
 
-#   db_subnet_group_name = aws_db_subnet_group.default.name
-#   vpc_security_group_ids = [
-#     aws_security_group.pg_security_group.id
-#   ]
-# }
+resource "aws_db_instance" "argoflow-db-instance" {
+  identifier = "argoflow-${module.vpc.name}-db"
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "postgres"
+  engine_version = "12.5"
+  instance_class = var.db_instance_type
+  name = "argoflow_db"
+  username = "postgres"
+  password = random_password.db_password.result
 
-# locals {
-#   server_subnet = var.private_subnets_ids[0]
-# }
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [
+    aws_security_group.pg_security_group.id
+  ]
+}
--- terrraform/vpc.tf
+++ terrraform/vpc.tf
@@ -2,7 +2,8 @@ data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-
+  version = "3.1.0"
+  
   name                 = "${var.cluster_name}-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
