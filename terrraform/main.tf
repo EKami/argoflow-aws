31b422f9153b595f85b7bf1544788088baf82f76
Tuatini <email@tuatini.me>
2021-06-20 10:44:24 +0200

    Add helm provider

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -38,6 +38,19 @@ provider "aws" {
   region                  = var.aws_region
 }
 
+provider "helm" {
+  # Redundant, find a way to merge with the one above
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
 # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
 module "eks" {
   source                  = "terraform-aws-modules/eks/aws"
